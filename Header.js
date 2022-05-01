69d418e8bb8a06b000c6c3f7ca179df33df4e795
Tarun kumar chourasiya <102983202+tarunhim@users.noreply.github.com>
2022-05-01 16:09:24 +0530

    Create Header.js

---
+++ Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+export default function Header() {
+    return (
+        <div className = "Header">
+        <h1> Reacts.js</h1>
+        <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+        </ul>
+        </div>
+    );
+}

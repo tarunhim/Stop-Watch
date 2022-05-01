6987ec109565a64413076c42aeb0aade69b29fff
Tarun kumar chourasiya <102983202+tarunhim@users.noreply.github.com>
2022-05-01 16:09:57 +0530

    Create Footer.js

---
+++ Footer.js
@@ -0,0 +1,8 @@
+import React from "react";
+export default function() {
+    return(
+        <footer className = "Footer">
+        <small> 2021 Tarun development. All rights reserved.</small>
+        </footer>
+    );
+}

12017ea33a22abc938400940a0730d01996df777
Tarun kumar chourasiya <102983202+tarunhim@users.noreply.github.com>
2022-05-07 22:25:01 +0530

    Delete Footer.js

--- Footer.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-export default function() {
-    return(
-        <footer className = "Footer">
-        <small> 2021 Tarun development. All rights reserved.</small>
-        </footer>
-    );
-}
